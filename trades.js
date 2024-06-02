dbd6f01dae02b38c1cce8c6d10be0eeaa4a8b118
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-06-02 16:54:19 +0530

    added delete method

--- trades.js
+++ trades.js
@@ -38,7 +38,8 @@ app.use(morgan(logFormat,{stream:accessLogStream}));
  
  //get by id
  app.get("/trades/:id",(req,res)=>{
-    const tradeId = req.params.id;
+
+    const tradeId = parseInt(req.params.id);
     console.log(tradeId)
     try {
         const data = JSON.parse(
@@ -74,6 +75,28 @@ app.use(morgan(logFormat,{stream:accessLogStream}));
   res.status(201).json({message : "trade added successfully"})
  })
 
+
+ // delete
+
+ app.delete("/trades/:id",(req,res)=>{
+    const tradeId = parseInt(req.params.id);
+     const dbPath = path.join(__dirname,"db.json");
+     try {
+        const db = JSON.parse(
+            fs.readFileSync(path.join(__dirname,"db.json"),"utf-8")
+        );
+        const updatedTrades = db.trades.filter((trade)=>trade.id !== tradeId);
+        if(updatedTrades.length === db.trades.length){
+            return res.status(404).send("Trade ID not found");
+        }
+        db.trades = updatedTrades ;
+        fs.writeFileSync(path.join(__dirname,"db.json"),JSON.stringify(db,null,2),"utf-8");
+        res.status(200).send(`Trade-${tradeId} is deleted`)
+     } catch (error) {
+        console.log('Error updating db.json',error);
+        res.status(500).send("internal server error")
+     }
+ })
  // server 
  const port = 8000
  app.listen(port,()=>{
